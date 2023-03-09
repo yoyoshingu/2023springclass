7926b26c5d33a01971673e87930c473100c1b80f
Y Lee <leo@shingu.ac.kr>
2023-03-09 14:53:59 +0900

    정적콘텐츠관리

---
+++ src/main/java/com/sg/leo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.sg.leo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/html")
+	public String html() {
+		System.out.println("HTML 파일이 요청됨");
+		return "redirect:starthere.html";
+	}
+	
+	@GetMapping("/img")
+	public String img() {
+		System.out.println("이미지파일이 요청됨");
+		return "redirect:img/m.png";
+		
+	}
+
+}
