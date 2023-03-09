5faf283c5272939a99352cfda12751678664efcf
Y Lee <leo@shingu.ac.kr>
2023-03-09 13:38:51 +0900

    첫번째 커밋

---
+++ src/main/java/com/sg/leo/SblogApplication.java
@@ -0,0 +1,16 @@
+package com.sg.leo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SblogApplication.class, args);
+	}
+	
+	// 2023.3.9 스프링수업시간에 만듬
+	// 이용희
+
+}
