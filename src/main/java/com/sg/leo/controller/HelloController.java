9577600a7d0fb3b7fd4144bed1b2e4306b3a1464
Y Lee <leo@shingu.ac.kr>
2023-03-09 15:14:13 +0900

    동적콘텐츠관리중

--- src/main/java/com/sg/leo/controller/HelloController.java
+++ src/main/java/com/sg/leo/controller/HelloController.java
@@ -17,5 +17,11 @@ public class HelloController {
 		return "redirect:img/m.png";
 		
 	}
+	
+	@GetMapping("/jsp")
+	public String jsp() {
+		System.out.println("JSP파일이 요청됨");
+		return "hellojsp";
+	}
 
 }
